9112d98d2a7eae8bffeccc0ae8f51ea49ad5d1b1
Ravi <ravi.valecha@qantasloyalty.com>
2019-04-03 12:45:00 +1100

    Modified for adding like button component on react

---
+++ amplifyapp/src/ButtonLike.js
@@ -0,0 +1,30 @@
+'use strict';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const e = React.createElement;
+
+class Ravi extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+// ... the starter code you pasted ...
+
+//const domContainer = document.querySelector('#like_button_container');
+//ReactDOM.render(e(LikeButton), domContainer);
+export default Ravi;
